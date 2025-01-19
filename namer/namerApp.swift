870a580ba756bbd5ae674b3abe2bdca6aabf6f47
Shahin shokoui <shahin@shahin.de>
2025-01-19 23:24:01 +0100

    Initial Commit

---
+++ namer/namerApp.swift
@@ -0,0 +1,17 @@
+//
+//  namerApp.swift
+//  namer
+//
+//  Created by Shahin on 19.01.25.
+//
+
+import SwiftUI
+
+@main
+struct namerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ namerTests/namerTests.swift
@@ -0,0 +1,17 @@
+//
+//  namerTests.swift
+//  namerTests
+//
+//  Created by Shahin on 19.01.25.
+//
+
+import Testing
+@testable import namer
+
+struct namerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
