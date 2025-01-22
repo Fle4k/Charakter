9755b2f84eb0a8a71045bc5b3c86801b6d3081e0
Shahin shokoui <shahin@shahin.de>
2025-01-22 16:40:40 +0100

    v.01.1

---
+++ namer/DeviceShakeViewModifier.swift
@@ -0,0 +1,35 @@
+import SwiftUI
+
+// Shake gesture detector
+struct DeviceShakeViewModifier: ViewModifier {
+    let action: () -> Void
+    
+    func body(content: Content) -> some View {
+        content
+            .onAppear()
+            .onReceive(NotificationCenter.default.publisher(for: UIDevice.deviceDidShakeNotification)) { _ in
+                action()
+            }
+    }
+}
+
+// Extend View to make the modifier easier to use
+extension View {
+    func onShake(perform action: @escaping () -> Void) -> some View {
+        self.modifier(DeviceShakeViewModifier(action: action))
+    }
+}
+
+// Extend UIDevice to create the shake notification
+extension UIDevice {
+    static let deviceDidShakeNotification = Notification.Name(rawValue: "deviceDidShakeNotification")
+}
+
+// Extend UIWindow to detect the shake gesture
+extension UIWindow {
+    open override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if motion == .motionShake {
+            NotificationCenter.default.post(name: UIDevice.deviceDidShakeNotification, object: nil)
+        }
+    }
+}
